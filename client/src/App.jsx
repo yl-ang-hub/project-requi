ebdb103df5328313fa9e86d9df5a0a559c7ec4d9
YL Ang <skya.ayl@gmail.com>
2025-09-09 16:38:52 +0800

    Update sidebar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { use, useState } from "react";
 import NavBar from "./components/NavBar";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
+import AuthCtx from "./components/context/authContext";
 
 function App() {
+  const authCtx = use(AuthCtx);
+  const [accessToken, setAccessToken] = useState("");
+  const [username, setUsername] = useState("");
+  const [userId, setUserId] = useState("");
+
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <NavBar>
-        <Routes>
-          <Route path="/" element={<Navigate to="/login" replace />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </NavBar>
+      <AuthCtx.Provider
+        value={{
+          accessToken,
+          setAccessToken,
+          username,
+          setUsername,
+          userId,
+          setUserId,
+        }}>
+        <div className="flex">
+          <div className="max-w-sm">
+            <NavBar></NavBar>
+          </div>
+          <div className="w-full flex bg-red-100">
+            <Routes>
+              <Route path="/" element={<Navigate to="/login" replace />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </div>
+        </div>
+      </AuthCtx.Provider>
     </ThemeProvider>
   );
 }
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -6,17 +6,30 @@ import {
   SidebarFooter,
   SidebarGroup,
   SidebarHeader,
+  SidebarMenuItem,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarGroupLabel,
+  SidebarGroupContent,
 } from "@/components/ui/sidebar";
 import { ModeToggle } from "./ModeToggle";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "./ui/dropdown-menu";
+import { DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
+import { ChevronDown } from "lucide-react";
+import { Link } from "react-router-dom";
 
 export default function NavBar(props) {
   return (
-    <SidebarProvider>
+    <SidebarProvider className="bg-green-300">
       <AppSidebar />
-      <main>
+      <main className="bg-blue-300">
         <SidebarTrigger />
-        {props.children}
       </main>
+      <div>{props.children}</div>
     </SidebarProvider>
   );
 }
@@ -29,14 +42,142 @@ function AppSidebar() {
           <ModeToggle />
           <span className="ml-3">REQUI APP</span>
         </div>
-        <div>Shrek - Superuser</div>
+
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton>
+                  Shrek - Superuser
+                  <ChevronDown className="ml-auto" />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+
+              <DropdownMenuContent side="bottom">
+                <DropdownMenuItem>Edit my profile</DropdownMenuItem>
+                <DropdownMenuItem>Sign out</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
       </SidebarHeader>
+
       <SidebarContent className="my-3 mx-3">
-        <SidebarGroup>Dashboard</SidebarGroup>
-        <SidebarGroup>Requisition</SidebarGroup>
-        <SidebarGroup>Purchase Orders</SidebarGroup>
-        <SidebarGroup>Suppliers</SidebarGroup>
+        <SidebarGroup>
+          <SidebarGroupLabel>My Links</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <Link to="/">Dashboard</Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+
+        {/* REQUISITION NAV */}
+        <SidebarGroup>
+          <SidebarGroupLabel>Requisition</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <Link to="/">Create New PR</Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <Link to="/">Pending PR</Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <Link to="/">Completed PR</Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+
+        {/* PURCHASE ORDERS NAV */}
+        <SidebarGroup>
+          <SidebarGroupLabel>Purchase Orders</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <Link to="/">Pending POs</Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <Link to="/">Completed POs</Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <Link to="/">Search POs</Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+
+        {/* SUPPLIERS NAV */}
+        <SidebarGroup>
+          <SidebarGroupLabel>Suppliers</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <Link to="/">Add New Supplier</Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <Link to="/">Update Supplier Info</Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <Link to="/">Search Suppliers</Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+
+        {/* ADMIN NAV */}
+        <SidebarGroup>
+          <SidebarGroupLabel>Administration</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <Link to="/">Add New User</Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <Link to="/">Edit Existing User</Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <Link to="/">Add New Role</Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <Link to="/">Edit Existing Role</Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
       </SidebarContent>
+
       <SidebarFooter className="my-3 mx-3">
         Sidebar Footer
         <br />
